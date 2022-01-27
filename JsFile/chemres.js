c09719170cdabda5460950376dd2e27fae7ac3da
Gupta2001 <80310618+Gupta2001@users.noreply.github.com>
2022-01-27 16:24:56 +0530

    Add files via upload

---
+++ JsFile/chemres.js
@@ -0,0 +1,171 @@
+const topic1 = [
+    {
+      itemText1: "Tutorial",
+      itemText2: "Theory-1",
+      link1: "https://drive.google.com/file/d/1865lnGByhSAsplVN6KorZFa3imGcY0qv/view",
+      linkText: "Motion Kota"
+    },
+    {
+      itemText1: "Questions",
+      itemText2: "Theory-2",
+      link1: "https://drive.google.com/file/d/1UO6pa1hq4HRyK73dknM1fwkvkbus_uD6/view",
+      linkText: "MathonGo"
+    }
+    
+    
+  ];
+  const topic2 = [
+    {
+      itemText1: "Tutorial",
+      itemText2: "Theory",
+      link1: "https://drive.google.com/open?id=1nHVPjblSIZt_e5Esm5CVNb2HQCN3HdK_",
+      linkText: "Motion Kota"
+    },
+    {
+      itemText1: "Questions",
+      itemText2: "Motion in 1-D",
+      link1: "https://bit.ly/3yFuOej",
+      linkText: "MathonGo"
+    },
+    {
+      itemText1: "Questions",
+      itemText2: "Motion in 2-D",
+      link1: "https://bit.ly/36jBmCX",
+      linkText: "MathonGo"
+    }
+   
+  ];
+  const topic3 = [
+    {
+      itemText1: "Tutorial",
+      itemText2: "Theory-1",
+      link1: "https://drive.google.com/open?id=1x_mfj4gkw7NV7yw1Ax3_qH_4F9E-0VEl",
+      linkText: "Motion Kota"
+    },
+    {
+      itemText1: "Tutorial",
+      itemText2: "Theory-2",
+      link1: "https://drive.google.com/open?id=1Z6HVIjxJu-SZiYw9kfUOZ6EGG_OguGHx",
+      linkText: "Motion Kota"
+    },
+    {
+      itemText1: "Questions",
+      itemText2: "NLM and Friction",
+      link1: "https://bit.ly/3wq8ljG",
+      linkText: "MathonGO"
+    }
+  ];
+  const topic4 = [
+    {
+      itemText1: "Tutorial",
+      itemText2: "Theory",
+      link1: "https://drive.google.com/open?id=1rALSNgZNjTjTLdtnTlVZAQS8o2Oc21jl",
+      linkText: "Motion Kota"
+    },
+    {
+      itemText1: "Questions",
+      itemText2: "CM and WPE",
+      link1: "https://bit.ly/2TNi4Du",
+      linkText: "MathonGO"
+    }
+  ];
+  const topic5 = [
+    {
+      itemText1: "Tutorial",
+      itemText2: "Theory",
+      link1: "https://drive.google.com/open?id=1sLwEqHDDEfXqb7n5OjHd0trrizTmH3-0",
+      linkText: "Motion Kota"
+    },
+    {
+      itemText1: "Questions",
+      itemText2: "Center of Mass Momentum Conservation and Collision",
+      link1: "https://bit.ly/3qW3nKp",
+      linkText: "MathonGO"
+    }
+  ];
+  const topic6 = [
+    {
+      itemText1: "Tutorial",
+      itemText2: "Theory",
+      link1: "https://drive.google.com/open?id=1XeplYaX3PD1uOGeDwTvQJpEmAvm9titW",
+      linkText: "Motion Kota"
+    },
+    {
+      itemText1: "Questions",
+      itemText2: "Roatation Motion",
+      link1: "https://bit.ly/3xqwzLX",
+      linkText: "MathonGO"
+    }
+  ];
+  const topic7 = [
+    {
+      itemText1: "Tutorial",
+      itemText2: "Theory",
+      link1: "https://drive.google.com/open?id=1aeMRaJGubLmSXpwA8X7nqgWmca4PBsG9",
+      linkText: "Motion Kota"
+    },
+    {
+      itemText1: "Questions",
+      itemText2: "Roatation Motion",
+      link1: "https://bit.ly/3xqwzLX",
+      linkText: "MathonGO"
+    }
+  ];
+  const topic8 = [
+    {
+      itemText1: "Tutorial",
+      itemText2: "Theory-1",
+      link1: "https://drive.google.com/open?id=149HruqDQHcjq0A0jCqfxeIUUz-B24dcq",
+      linkText: "Motion Kota"
+    },
+    {
+      itemText1: "Tutorial",
+      itemText2: "Theory-2",
+      link1: "https://drive.google.com/open?id=120bUCerDqsHGnzunzEoWuAvtcx4PZKJr",
+      linkText: "Motion Kota"
+    }
+  ];
+  const topic9 = [
+    {
+      itemText1: "Tutorial",
+      itemText2: "Graph-II",
+      link1: "https://docs.google.com/document/d/1ev-Juh-s-XO9DTAjzxEGLX26wQMctiKIGyT0yKLtBPw/edit?usp=sharing",
+      linkText: "DOC"
+    },
+    {
+      itemText1: "Tutorial",
+      itemText2: "Graph-II",
+      link1: "https://docs.google.com/document/d/1Z268CVkot0ZhUx0xyyA_Bkok6HSPi9UcvPapDNtiKQ0/edit?usp=sharing",
+      linkText: "DOC"
+    }
+  ];
+  const topic10 = [
+    {
+      itemText1: "Tutorial",
+      itemText2: "Miscellaneous-I",
+      link1: "https://cp-algorithms.com/",
+      linkText: "CP-Algo"
+    },
+    {
+      itemText1: "Tutorial",
+      itemText2: "Miscellaneous-I",
+      link1: "https://codeforces.com/edu/course/2",
+      linkText: "cf-edu"
+    }
+  ];
+  const topic11 = [
+    {
+      itemText1: "Tutorial-11",
+      itemText2: "Miscellaneous-I",
+      link1: "https://cp-algorithms.com/",
+      linkText: "CP-Algo"
+    },
+    {
+      itemText1: "Tutorial",
+      itemText2: "Miscellaneous-I",
+      link1: "https://codeforces.com/edu/course/2",
+      linkText: "cf-edu"
+    }
+  ];
+  
+  
---
+++ JsFile/mathres.js
@@ -0,0 +1,147 @@
+const topic1 = [
+    {
+      itemText1: "Tutorial",
+      itemText2: "Theory and Questions",
+      link1: "https://drive.google.com/open?id=1nci4QHQZj56BNe2khHQZAbhFEGyokysN",
+      linkText: "Motion Kota"
+    },
+    {
+      itemText1: "Questions",
+      itemText2: "Questions Solutions",
+      link1: "https://drive.google.com/open?id=1d2Imi1FGyBZuU6L95QSohTsaCIhBpBQm",
+      linkText: "Motion Kota"
+    }
+    
+    
+  ];
+  const topic2 = [
+    {
+        itemText1: "Tutorial",
+        itemText2: "Theory and Questions",
+        link1: "https://drive.google.com/open?id=1ISamsnk8IyU05JOrWzAPPUIepsPC3Yn7",
+        linkText: "Motion Kota"
+      },
+      {
+        itemText1: "Questions",
+        itemText2: "Questions Solutions",
+        link1: "https://drive.google.com/open?id=1s-Q0uL9X7Gy3Vw9Iq9r3gZ43G-8S9P_8",
+        linkText: "Motion Kota"
+      }
+      
+  ];
+  const topic3 = [
+    {
+        itemText1: "Tutorial",
+        itemText2: "Theory and Questions",
+        link1: "https://drive.google.com/open?id=1T-YZhT4vaKJ8ZTbp-74fJTfM8QyBatEA",
+        linkText: "Motion Kota"
+      },
+      {
+        itemText1: "Questions",
+        itemText2: "Questions Solutions",
+        link1: "https://drive.google.com/open?id=1TSiO9qZ8jkRI4zduV4Pi5jK7Hwbg18tO",
+        linkText: "Motion Kota"
+      }
+      
+  ];
+  const topic4 = [
+    {
+        itemText1: "Tutorial",
+        itemText2: "Theory and Questions",
+        link1: "https://drive.google.com/open?id=1R7yR-rtHUKzUYMETkopBysuwhWUW16jy",
+        linkText: "Motion Kota"
+      },
+      {
+        itemText1: "Questions",
+        itemText2: "Questions Solutions",
+        link1: "https://drive.google.com/open?id=1y43bRfmtEqwZL4K7DHzquyflYFUS5Uwi",
+        linkText: "Motion Kota"
+      }
+  ];
+  const topic5 = [
+    {
+        itemText1: "Tutorial",
+        itemText2: "Theory and Questions",
+        link1: "https://drive.google.com/open?id=1Q8C2kH8CoObj_VcYefGaGXPWRz1gi53D",
+        linkText: "Motion Kota"
+      },
+      {
+        itemText1: "Questions",
+        itemText2: "Questions Solutions",
+        link1: "https://drive.google.com/open?id=13ddY7OpbviPsirl69MfqLmq_PjxzPRn9",
+        linkText: "Motion Kota"
+      }
+  ];
+  const topic6 = [
+    {
+        itemText1: "Tutorial",
+        itemText2: "Theory and Questions",
+        link1: "https://drive.google.com/open?id=1M9YcSEjiSOfjXShERKA4rpOdUTv1l8JI",
+        linkText: "Motion Kota"
+      },
+      {
+        itemText1: "Questions",
+        itemText2: "Questions Solutions",
+        link1: "https://drive.google.com/open?id=1NbOkgjxmffR9sz8jIs5Ft5jWKGRRFNm0",
+        linkText: "Motion Kota"
+      }
+  ];
+  const topic7 = [
+    {
+        itemText1: "Tutorial",
+        itemText2: "Theory and Questions",
+        link1: "https://drive.google.com/open?id=1wt-_monpTdGVFQ-0uqZgIvR-gXDqL3f6",
+        linkText: "Motion Kota"
+      },
+      {
+        itemText1: "Questions",
+        itemText2: "Questions Solutions",
+        link1: "https://drive.google.com/open?id=1-33P6LPKvy7K5OEcR8LM5tZbMoHgFpcA",
+        linkText: "Motion Kota"
+      }
+  ];
+  const topic8 = [
+    {
+        itemText1: "Tutorial",
+        itemText2: "Theory and Questions",
+        link1: "https://drive.google.com/open?id=1vZvcpN42zciIDsKC0zHRbIW2Fh6W4ZfN",
+        linkText: "Motion Kota"
+      },
+      {
+        itemText1: "Questions",
+        itemText2: "Questions Solutions",
+        link1: "https://drive.google.com/open?id=1UFZ8LvQRD3Bjr07Hgi_PQwamI3j6810-",
+        linkText: "Motion Kota"
+      }
+  ];
+  const topic9 = [
+    {
+        itemText1: "Tutorial",
+        itemText2: "Theory and Questions",
+        link1: "https://drive.google.com/open?id=1jpiM1ZYosNhCRWpPg5P3pXLJb3bvPb7w",
+        linkText: "Motion Kota"
+      },
+      {
+        itemText1: "Questions",
+        itemText2: "Questions Solutions",
+        link1: "https://drive.google.com/open?id=1KhR-feby1UUewmSorPvmQE_5tH46fPCH",
+        linkText: "Motion Kota"
+      }
+  ];
+  const topic10 = [
+    {
+        itemText1: "Tutorial",
+        itemText2: "Theory and Questions",
+        link1: "https://drive.google.com/open?id=1lZ6NfFHIV85NiobxWvoclLjg0Ufbwcj9",
+        linkText: "Motion Kota"
+      },
+      {
+        itemText1: "Questions",
+        itemText2: "Questions Solutions",
+        link1: "https://drive.google.com/open?id=1LBoOLUxmLExUtlLbnFc0rRcxQZhR7ssd",
+        linkText: "Motion Kota"
+      }
+  ];
+  
+  
+  
